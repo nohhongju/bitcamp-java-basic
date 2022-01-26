ecf46c2f610653fe32fc2cb2d45be11bda90fe89
nohhongju <ghdwn49@gmail.com>
2022-01-26 16:20:45 +0900

    16:20 삼항연산자 사용

--- java/com/example/demo/login/LoginApp.java
+++ java/com/example/demo/login/LoginApp.java
@@ -29,10 +29,13 @@ public class LoginApp {
         }else {
             res = String.format("%s 의 ID는 맞고, 비번 %s 가 틀립니다. 로그인 실패 ", this.id, this.pw);
         }*/
+        /*
         switch (pw){
             case "abc": res = String.format("%s 님의 비번 %s 가 맞습니다. 로그인 성공", this.name, this.pw); break;
             default: res = String.format("%s 의 ID는 맞고, 비번 %s 가 틀립니다. 로그인 실패 ", this.id, this.pw); break;
-        }
+        }*/
+        res = (pw.equals(PASSWORD)) ? String.format("%s 님의 비번 %s 가 맞습니다. 로그인 성공", this.name, this.pw)
+                : String.format("%s 의 ID는 맞고, 비번 %s 가 틀립니다. 로그인 실패 ", this.id, this.pw);
         return res;
 
 
